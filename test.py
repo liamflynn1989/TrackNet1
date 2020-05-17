688b447ed0a1d217258ca5d4fc38f5d59691c01d
liamflynn1989 <liamflynn1989@gmail.com>
2020-05-17 23:55:48 +1000

    third commit

---
+++ test.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 10:38:40 2020
+
+@author: liamf
+"""
+
+import numpy as np
+import cv2
+import itertools
+import csv
+from collections import defaultdict
+import os
+
+
+import argparse
+import Models , LoadBatches
+from tensorflow.keras import optimizers
+from tensorflow.keras.utils import plot_model
+
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+
+import tensorflow as tf
+tf.test.is_gpu_available() # True/False
+
+height = 360
+width = 640
+
+
+direct = '~/'
+path = 'Dataset/Dataset/game4/Clip7/0177.jpg'
+
+
+img = cv2.imread(os.path.join(direct,path), 1)
+ #resize it 
+img = cv2.resize(img, ( width , height ))
+#input must be float type
+img = img.astype(np.float32)
+
+
+print("success")
+
+
+
+python  predict_video.py  --save_weights_path=weights/model.3 --input_video_path="/home/paperspace/KenMug.mp4" --n_classes=256 
